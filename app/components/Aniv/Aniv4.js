ca9ea26e306646aef1f6b3f6a42ad0b2967a1058
pythonthm <pythonthm@gmail.com>
2023-10-24 10:09:51 +0900

    1024

--- app/components/Aniv/Aniv4.js
+++ app/components/Aniv/Aniv4.js
@@ -3,7 +3,13 @@ const Aniv4 = () => {
     <>
       <div className="main-container">
         <h2>
-          <span>9月3日</span>(抗日戦争勝利記念日)
+          <span>9月3日</span>
+          <a
+            className="kounichi"
+            href="http://jp.china-embassy.gov.cn/jpn/zrdt_1/201403/t20140303_10433600.htm"
+          >
+            (抗日戦争勝利記念日)
+          </a>
         </h2>
       </div>
     </>
